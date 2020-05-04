7e19cae69cccc4cbc4f52d7042afc0dcd98897fd
Casey The Great <casey@caseypitman.com>
2020-05-04 13:54:23 -0500

    Built navbar. Set jumbo background.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//jQuery
+$(document).ready(function(){
+    //Collapse menu bar after click on mobile
+
+    $(".nav-link").on("click", function(){
+        $(".navbar-collapse").collapse("hide");
+    });
+
+
+
+}); //end jQuery
+
