207627e5ba1f47f998a13d3cac5a69f6357e42ad
Casey The Great <casey@caseypitman.com>
2020-05-13 11:45:20 -0500

    working on form redirect

--- js/scripts.js
+++ js/scripts.js
@@ -8,23 +8,6 @@ $(document).ready(function(){
 
 
 
-    //form stuff
-
-    var message = "";
-
-$("#submit-msg").on("click", function() {
-    message = $("#contactform").serialize();
-    $.ajax({
-        url: "//formspree.io/dynamicrealities@gmail.com", 
-        method: "POST",
-        data: {message: message},
-        dataType: "json"
-    });
-    alert('Thanks for the email, we\'ll be in touch promptly.');
-    return false;
-});
-
-
 
 }); //end jQuery
 
