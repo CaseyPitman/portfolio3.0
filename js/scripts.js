db376cc72bda9b0f7e9da02bd57d0468f34ebd7e
Casey The Great <casey@caseypitman.com>
2020-05-21 17:01:20 -0500

    Type animation mostly done. Timing needs refining.

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,72 @@ $(document).ready(function(){
     //TYPEWRITER EFFECT FOR HEADER
 
     //Blinking Cursor
-    function cursorBlink() {
-        $('#cursor').fadeOut(500);
-        $('#cursor').fadeIn(500);
-    }
-    setInterval(cursorBlink, 1000);
+    
+
+    //Typing
+    let text1="Hello."
+    let i=0;
+    
+        function typeWriter1() {
+            function cursorBlink() {
+                $('#cursor1').fadeOut(500);
+                $('#cursor1').fadeIn(500);
+            }
+            setInterval(cursorBlink, 1000);
+            if (i < text1.length) {
+            document.getElementById("jumbo-head-1").innerHTML += text1.charAt(i);
+            i++;
+            Math.floor(Math.random()*201)+10;
+            setTimeout(typeWriter1, 222);
+            }   
+            setTimeout(typeWriter2, 2400);  
+        } 
 
   
 
+    let text2="My name is Casey Pitman."
+    let j=0;
 
+         function typeWriter2() {
+            $("#cursor1").remove();
+            $("#cursor2").toggle();
+            function cursorBlink() {
+                $('#cursor2').fadeOut(500);
+                $('#cursor2').fadeIn(500);
+            }
+            setInterval(cursorBlink, 1000);
+            if (j < text2.length) {
+            document.getElementById("jumbo-head-2").innerHTML += text2.charAt(j);
+            j++;
+            setTimeout(typeWriter2, 700);
+            }   
+            setTimeout(typeWriter3, 5000);
+        }
 
+        let text3="I'm a web developer who loves to solve problems."
+        let k=0;
+        function typeWriter3() {
+            $("#cursor2").remove();
+            $("#cursor3").toggle();
+            function cursorBlink() {
+                $('#cursor3').fadeOut(500);
+                $('#cursor3').fadeIn(500);
+            }
+            setInterval(cursorBlink, 1000);
+            if (k < text3.length) {
+            document.getElementById("jumbo-head-3").innerHTML += text3.charAt(k);
+            k++;
+           let speed3=Math.floor(Math.random()*501)+10;
+            setTimeout(typeWriter3, 1500);
+            }  
+            setTimeout(clearCursor, 7000);
+        
+        } 
+
+    function clearCursor(){
+        $("#cursor3").remove();
+    }    
+  typeWriter1();
 
 }); //end jQuery
 
