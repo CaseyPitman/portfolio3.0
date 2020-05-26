cfe6e9dacf5b98834922154680421bfc9b62e4fe
Casey The Great <casey@caseypitman.com>
2020-05-26 14:07:59 -0500

    Optimized images.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
 //jQuery
 $(document).ready(function(){
     //Collapse menu bar after click on mobile
-
     $(".close-bar").on("click", function(){
         $(".navbar-collapse").collapse("hide");
     });
 });
 
     //TYPEWRITER EFFECT FOR HEADER
-
     let text1="Hello."
     let i=0;
-    
+    //LINE ONE OF TEXT
     function typeWriter1() {
         if (i < text1.length) {
         document.getElementById("jumbo-head-1").innerHTML += text1.charAt(i);
@@ -20,7 +18,7 @@ $(document).ready(function(){
         }   
         setTimeout(typeWriter2, 1000);  
     } 
-
+    //LINE TWO OF TEXT
     let text2="My name is Casey."
     let j=0;
 
@@ -34,7 +32,7 @@ $(document).ready(function(){
         }   
         setTimeout(typeWriter3, 2000);
         }
-
+    //LINE THREE OF TEXT
     let text3="I'm a web developer who loves to solve problems."
     let k=0;
     function typeWriter3() {
@@ -47,10 +45,10 @@ $(document).ready(function(){
         }  
         setTimeout(clearCursor, 2000);
     } 
-
+    //REMOVE CURSOR
     function clearCursor(){
         $("#cursor3").remove();
     }
-
+//FIRES TYPEWRITER AFTER PAGE LOAD
 $(window).on("load", typeWriter1);
 
